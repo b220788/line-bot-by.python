982cc1d81cb624ff89a5064d4fd6696e0bc0ee36
Yuan-Technology <b220788@gmail.com>
2024-06-09 04:13:52 +0800

    添加更多設備類型

--- line_bot.py
+++ line_bot.py
@@ -45,6 +45,8 @@ def pretty_echo(event):
     global control_Device
     global control_ID
     global control_Status
+    global status
+    global Device
 
     for i in event.message.text:
         message_get += i
@@ -67,90 +69,58 @@ def pretty_echo(event):
             TextSendMessage(text="您好，加入客服中心，回報問題後會有專人為您服務 \n連結:https://lin.ee/hxeuVAO")
         )
             
-    else :        
-        if message_get =="開循環風扇":
-            FlexMessage = json.load(open('FlexMessage.json','r',encoding='utf-8'))
+    else :
+        if "開" in message_get:
             control_Status="ON"
-            control_Device="FAN"
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("循環風扇",FlexMessage))
-        elif message_get=="關循環風扇":
+            status="開"
+        elif "關" in message_get:
             control_Status="OFF"
-            control_Device="FAN"
-            FlexMessage = json.load(open('FlexMessage.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("循環風扇",FlexMessage))
-        if message_get =="開負壓風扇":
-            control_Status="ON"
-            control_Device="NP_FAN"
-            FlexMessage = json.load(open('FlexMessage_NP_FAN.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("負壓風扇",FlexMessage))
-        elif message_get=="關負壓風扇":
-            control_Status="OFF"
-            control_Device="NP_FAN"
-            FlexMessage = json.load(open('FlexMessage_NP_FAN.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("負壓風扇",FlexMessage))
-        if message_get =="開噴霧馬達":
-            control_Status="ON"
-            control_Device="WATER"
-            FlexMessage = json.load(open('FlexMessage_WATER.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴霧馬達",FlexMessage))
-        elif message_get =="關噴霧馬達":
-            control_Status="OFF"
-            control_Device="WATER"
-            FlexMessage = json.load(open('FlexMessage_WATER.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴霧馬達",FlexMessage))
-
-        if message_get =="開養液":
-            control_Status="ON"
-            control_Device="nutrien"
-            FlexMessage = json.load(open('FlexMessage_N_S.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("養液供應",FlexMessage))
-        elif message_get =="關養液":
-            control_Status="OFF"
-            control_Device="nutrien"
-            FlexMessage = json.load(open('FlexMessage_N_S.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("養液供應",FlexMessage))
-
-
-        if message_get =="開噴藥馬達":
-            control_Status="ON"
-            control_Device="SPPRAY"
-            FlexMessage = json.load(open('FlexMessage_SPRAY.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴藥馬達",FlexMessage))
-        elif message_get =="關噴藥馬達":
-            control_Status="OFF"
-            control_Device="SPPRAY"
-            FlexMessage = json.load(open('FlexMessage_SPRAY.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴藥馬達",FlexMessage))
-
-
-
-        if message_get =="開天窗":
-            FlexMessage = json.load(open('FlexMessage_SKY.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("天窗控制",FlexMessage))
-        elif message_get =="關天窗":
-            FlexMessage = json.load(open('FlexMessage_SKY.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("天窗控制",FlexMessage))
-            
-        if message_get =="開遮陰":
-            FlexMessage = json.load(open('FlexMessage_SHADOW.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("遮陰控制",FlexMessage))
-        elif message_get =="關遮陰":
-            FlexMessage = json.load(open('FlexMessage_SHADOW.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("遮陰控制",FlexMessage))
-
-        if message_get =="開捲揚":
-            FlexMessage = json.load(open('FlexMessage_WINCH.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("捲揚控制",FlexMessage))
-        elif message_get =="關捲揚":
-            FlexMessage = json.load(open('FlexMessage_WINCH.json','r',encoding='utf-8'))
-            line_bot_api.reply_message( event.reply_token, FlexSendMessage("捲揚控制",FlexMessage))
+            status="關"
+        elif "停" in message_get:
+            control_Status="STOP"
+            status="停"
         
-    
-        #else:
-        #    line_bot_api.reply_message(
-        #        event.reply_token,
-        #        TextSendMessage(text=message_get)
-        #    )
+        if "循環風扇" in message_get :
+            control_Device="FAN"
+            Device="循環風扇"
+            FlexMessage = json.load(open('FlexMessage.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("循環風扇",FlexMessage))
+        elif "負壓風扇" in message_get :
+            control_Device="NP_FAN"
+            Device="負壓風扇"
+            FlexMessage = json.load(open('FlexMessage_NP_FAN.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("負壓風扇",FlexMessage))
+        elif "噴霧馬達" in message_get :
+            control_Device="WATER"
+            Device="噴霧馬達"
+            FlexMessage = json.load(open('FlexMessage_WATER.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴霧馬達",FlexMessage))
+        elif "養液馬達" in message_get :
+            control_Device="nutrien"
+            Device="養液馬達"
+            FlexMessage = json.load(open('FlexMessage_N_S.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("養液供應",FlexMessage))
+        elif "噴藥馬達" in message_get :
+            control_Device="SPPRAY"
+            Device="噴藥馬達"
+            FlexMessage = json.load(open('FlexMessage_SPRAY.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴藥馬達",FlexMessage))
+        elif "天窗" in message_get :
+            control_Device="SKY"
+            Device="天窗"
+            FlexMessage = json.load(open('FlexMessage_SKY.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("天窗控制",FlexMessage))
+        elif "遮陰" in message_get :
+            control_Device="SHADOW"
+            Device="遮陰"
+            FlexMessage = json.load(open('FlexMessage_SHADOW.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("遮陰控制",FlexMessage))
+        elif "捲揚" in message_get :
+            control_Device="WINCH"
+            Device="捲揚"
+            FlexMessage = json.load(open('FlexMessage_WINCH.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("捲揚控制",FlexMessage))
+
 
 """else :
         if message_get == "報到" :
@@ -172,35 +142,14 @@ def handle_postback(event):
     ts = event.postback.data
     if ts!= "richmenu-changed-to-a" and ts!= "richmenu-changed-to-b":
         #test(control_ID,FAN,ts)
-        
         print(ts+","+control_Device+","+control_ID+","+control_Status)
-        if control_Status =="ON" :
-            status="開"
-        else :
-            status="關"
-        if control_Device=="FAN" :
-            Device="循環風扇"
-        elif control_Device=="NP_FAN":
-            Device="負壓風扇"
-        elif control_Device=="WATER":
-            Device="噴霧馬達"
-        
         response = requests.get(url="http://yuanmqtt.ddns.net:1886/"+control_Device+"?Device="+control_Device+"&NUM="+ts+"&Status="+control_Status+"&userID="+control_ID)
         print(response.status_code)
         print(response.text)
         if response.status_code==200:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=Device+ts+status)
+                TextSendMessage(text=Device+ts+"號 "+status)
                 )
-
-"""
-    line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=ts)
-            )
-"""
-
-
 if __name__ == "__main__":
     app.run()
