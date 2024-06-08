9130dd90af34006bb3377be4f043f8bdddf0fd92
Yuan-Technology <b220788@gmail.com>
2024-06-09 04:22:47 +0800

    添加layout檔案與新增設備類型變數、控制代號

--- line_bot.py
+++ line_bot.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import os
 import json
 import requests
+import time
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -47,16 +48,18 @@ def pretty_echo(event):
     global control_Status
     global status
     global Device
+    global name
     
     response=requests.get('http://yuanmqtt.ddns.net:1886/sql')
-        if response.status_code ==200 :
-            ID_LIST=json.loads(response.text)
-            num=len(ID_LIST)
-            for i in range(num):
-                if ID_LIST[i]["LINE_ID"] == event.source.user_id:
-                    control_ID=ID_LIST[i]["SITE_ID"]
-                    print(control_ID)
-                    break
+    if response.status_code == 200 :
+        ID_LIST=json.loads(response.text)
+        num=len(ID_LIST)
+        for i in range(num):
+            if ID_LIST[i]["LINE_ID"] == event.source.user_id:
+                control_ID=ID_LIST[i]["SITE_ID"]
+                name=ID_LIST[i]["name"]
+                print(control_ID)
+                break
 
     for i in event.message.text:
         message_get += i
@@ -68,16 +71,20 @@ def pretty_echo(event):
             TextSendMessage(text="您的ID為"+UserId)
         )
 
-    if control_ID != "null" :
+    if control_ID != "null" :#如果有對應的LINE_ID
 
 
         
         if message_get =="控制選單":
-                FlexMessage = json.load(open('FlexMessage_control.json','r',encoding='utf-8'))
-                line_bot_api.reply_message( event.reply_token, FlexSendMessage("控制選單",FlexMessage))
+        
+        
+
+            A='255'
+            FlexMessage = json.load(open('layout/ALL_MENU/FlexMessage_control_'+A+'.json','r',encoding='utf-8'))
+            line_bot_api.reply_message( event.reply_token, FlexSendMessage("控制選單",FlexMessage))
+
+
             
-            
-                
         elif message_get =="聯絡客服":
             line_bot_api.reply_message(
             event.reply_token,
@@ -96,64 +103,50 @@ def pretty_echo(event):
         if "循環風扇" in message_get :
             control_Device="FAN"
             Device="循環風扇"
-            FlexMessage = json.load(open('FlexMessage.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/FAN/FlexMessage_FAN_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("循環風扇",FlexMessage))
         elif "負壓風扇" in message_get :
             control_Device="NP_FAN"
             Device="負壓風扇"
-            FlexMessage = json.load(open('FlexMessage_NP_FAN.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/NP_FAN/FlexMessage_NP_FAN_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("負壓風扇",FlexMessage))
         elif "噴霧馬達" in message_get :
             control_Device="WATER"
             Device="噴霧馬達"
-            FlexMessage = json.load(open('FlexMessage_WATER.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/WATER/FlexMessage_WATER_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴霧馬達",FlexMessage))
         elif "養液馬達" in message_get :
             control_Device="nutrien"
             Device="養液馬達"
-            FlexMessage = json.load(open('FlexMessage_N_S.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/N_S/FlexMessage_N_S_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("養液供應",FlexMessage))
         elif "噴藥馬達" in message_get :
-            control_Device="SPPRAY"
+            control_Device="SPRAY"
             Device="噴藥馬達"
-            FlexMessage = json.load(open('FlexMessage_SPRAY.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/SPRAY/FlexMessage_SPRAY_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("噴藥馬達",FlexMessage))
         elif "天窗" in message_get :
             control_Device="SKY"
             Device="天窗"
-            FlexMessage = json.load(open('FlexMessage_SKY.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/SKY/FlexMessage_SKY_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("天窗控制",FlexMessage))
         elif "遮陰" in message_get :
             control_Device="SHADOW"
             Device="遮陰"
-            FlexMessage = json.load(open('FlexMessage_SHADOW.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/SHADOW/FlexMessage_SHADOW_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("遮陰控制",FlexMessage))
         elif "捲揚" in message_get :
             control_Device="WINCH"
             Device="捲揚"
-            FlexMessage = json.load(open('FlexMessage_WINCH.json','r',encoding='utf-8'))
+            FlexMessage = json.load(open('layout/WINCH/FlexMessage_WINCH_3.json','r',encoding='utf-8'))
             line_bot_api.reply_message( event.reply_token, FlexSendMessage("捲揚控制",FlexMessage))
-    else :
+    else : #如果沒有
         line_bot_api.reply_message(
         event.reply_token,
             TextSendMessage(text="您尚未擁有控制權，請在對話框輸入[報到]!")
         )
 
-"""else :
-        if message_get == "報到" :
-            print("取得id :")
-            UserId = event.source.user_id
-            print(UserId)
-            print(" ")
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您的ID為"+UserId)
-            )
-        else :
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您尚未擁有控制權，請在對話框輸入[報到]後取得您的ID，並連繫服務人員!")
-            )"""
+
 @handler.add(PostbackEvent)
 def handle_postback(event):
     ts = event.postback.data
@@ -168,5 +161,8 @@ def handle_postback(event):
                 event.reply_token,
                 TextSendMessage(text=Device+ts+"號 "+status)
                 )
+
+
+
 if __name__ == "__main__":
     app.run()
